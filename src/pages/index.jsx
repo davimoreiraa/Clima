1bd0e2d171740f7106873244ae65d256c89f7043
Davi Moreira <davimoreiraasantoss@gmail.com>
2022-07-31 19:05:57 -0300

    added _document.js

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -28,7 +28,7 @@ export default function Home() {
         />
         <link rel="icon" href="/sun.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400&display=swap" rel="stylesheet" />
       </Head>
       <main className={`${styles.content} d-flex justify-content-center`}>
