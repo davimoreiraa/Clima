4502244825a2634f2e0654d1f04c30f92b644208
Davi Moreira <davimoreiraasantoss@gmail.com>
2022-07-31 11:59:32 -0300

    stylized and added all infos to the climate box

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -37,35 +37,67 @@ export default function Home() {
             <div className={`${styles.input_container} col-10 col-md-7 `}>
               <input
                type="text" 
-               className="form-control" 
+               className={`form-control`} 
                id="city" 
                placeholder="Insira aqui a cidade"
                onChange={(e) => {
                 setCity(e.target.value)
-              }}
-              value={city}
+                }}
+                value={city}
                />
-               <button 
-               onClick={() => {
-                takeDataAPI()
-                setCityToBeExbithed(city)
-                console.log(data)
-                }}>
-                  Buscar
-                </button>
+               <div className={`d-flex justify-content-center`}>
+                <button 
+                  className={`${styles.search_btn} btn btn-secondary col-8 col-sm-7 col-lg-5 col-xl-3 `}
+                  type='button'
+                  onClick={() => {
+                    takeDataAPI()
+                    setCityToBeExbithed(city)
+                    console.log(data)
+                  }}>
+                    Buscar
+                 </button>
+               </div>
             </div>
           </div>
-          <article className={`${styles.climate_box}`}>
-            <div>
-              {data.current == undefined ? '' : <img src={data.current.condition.icon} /> }
-            </div>
-            <div className={`${styles.climate_infos}`}>
-              <h2>{cityToBeExbithed}</h2>
-              {data.current == undefined ? '' : <h2>{data.current.feelslike_c}</h2>}
-            </div>
-          </article>
-        </div>
-        <div>
+          <div className={`col-12 d-flex justify-content-center`}>
+            <article className={`${styles.climate_box} ${ data.current ? '' : 'd-none' } rounded-3 d-inline-flex`}>
+              <div>
+                <div className={`${styles.climate_infos}`}>
+                  { data.current == undefined ? '' : 
+                    <h2 className={`${styles.location}`}>
+                      {data.location.name}, {data.location.region} - {data.location.country} 
+                    </h2> 
+                  }
+                  <div className={`d-flex justify-content-center`}>
+                  <div className={`${styles.line} col-10`}></div>
+                  </div>
+                  <div className={`${styles.container} d-flex`}>
+                  { data.current == undefined ? '' :
+                   <h3 className={`${styles.temperature}`}>
+                      {data.current.temp_c}°C
+                    </h3>
+                  }
+                  { data.current == undefined ? '' :
+                   <h3 className={`${styles.feelslike}`}>
+                    Sensação: <strong>{data.current.feelslike_c}°C</strong>
+                    </h3>
+                  }
+                  </div>
+                  { data.current == undefined ? '' :
+                    <p className={`${styles.humidity}`}>Umidade: <strong>{data.current.humidity}%</strong></p>
+                  }
+                </div>
+                <div className={`d-flex justify-content-center`}>
+                  {data.current == undefined ? '' : <img src={data.current.condition.icon} /> }
+                  { data.current == undefined ? '' :
+                   <h3 className={`${styles.text}`}>
+                      {data.current.condition.text}
+                    </h3>
+                  }
+                </div>
+              </div>
+            </article>
+          </div>
         </div>
       </main>
     </div>
