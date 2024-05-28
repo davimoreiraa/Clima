8e747cdfc169f7a069106a1def44ef016c8c3dc9
unknown <davimoreiraasantoss@gmail.com>
2024-05-28 10:54:55 -0300

    now the temperature displayed is in celsius

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,17 +6,27 @@ import styles from './styles.module.css'
 
 /* ------------------ RESOURCES -------------------*/
 import axios from "axios"
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function Home() {
   const [city, setCity] = useState('')
   const [cityToBeExbithed, setCityToBeExbithed] = useState('')
   const [data, setData] = useState({}) 
+  const [temp, setTemp] = useState(0) 
+
+  useEffect(() => {
+    try {
+      setTemp((data.main.temp - 273.15).toFixed(2))
+    }
+    catch(e) {
+      console.log(e)
+    }
+  }, [data])
 
   const takeDataAPI = () => {
     return axios.get('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=9ea3ecdb37e62c29b1808d406c1a8fa1')
-        .then((response) => setData(response.data))
-        .catch((err) => console.log(err))
+    .then((response) => setData(response.data))
+    .catch((err) => console.log(err))
   }
 
   return (
@@ -27,7 +37,6 @@ export default function Home() {
           content="Website que exibe o clima de uma cidade escolhida"
         />
         <link rel="icon" href="/sun.png" />
-        
       </Head>
       <main className={`${styles.content} d-flex justify-content-center`}>
         <div className={`${styles.container} col-11 `}>
@@ -49,11 +58,12 @@ export default function Home() {
                <div className={`d-flex justify-content-center`}>
                 <button 
                   className={`${styles.search_btn} btn btn-secondary col-8 col-sm-7 col-lg-5 col-xl-3 `}
-                  type='button'
+                  type='button' 
                   onClick={() => {
                     takeDataAPI()
                     setCityToBeExbithed(city)
                     console.log(data)
+                    console.log(data.name)
                   }}>
                     Buscar
                  </button>
@@ -61,33 +71,33 @@ export default function Home() {
             </div>
           </div>
           <div className={`col-12 d-flex justify-content-center`}>
-            <article className={`${styles.climate_box} ${ data.current ? '' : 'd-none' } rounded-3 d-inline-flex`}>
+            <article className={`${styles.climate_box} ${ data.main ? '' : 'd-none' } rounded-3 d-inline-flex`}>
               <div>
                 <div className={`${styles.climate_infos}`}>
-                  { data.current == undefined ? '' : 
+                  { data.main == undefined ? '' : 
                     <h2 className={`${styles.location}`}>
-                      {data.city_name}
+                      {data.name}
                     </h2> 
                   }
                   <div className={`d-flex justify-content-center`}>
                   <div className={`${styles.line} col-10`}></div>
                   </div>
                   <div className={`${styles.container} d-flex`}>
-                  { data.current == undefined ? '' :
+                  { data.main == undefined ? '' :
                    <h3 className={`${styles.temperature}`}>
-                      {data.temp}°C
+                      {temp}°C
                     </h3>
                   }
                   </div>
-                  { data.current == undefined ? '' :
+                  { data.main == undefined ? '' :
                     <p className={`${styles.humidity}`}>Umidade: <strong>{data.humidity}%</strong></p>
                   }
                 </div>
                 {/* <div className={`d-flex justify-content-center`}>
-                  {data.current == undefined ? '' : <img src={data.current.condition.icon} alt ='condition'/> }
-                  { data.current == undefined ? '' :
+                  {data.main == undefined ? '' : <img src={data.main.condition.icon} alt ='condition'/> }
+                  { data.main == undefined ? '' :
                    <h3 className={`${styles.text}`}>
-                      {data.current.condition.text}
+                      {data.main.condition.text}
                     </h3>
                   }
                 </div> */}
